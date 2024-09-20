582778eb71dc231200e4e098cfca4293576af62e
Arttutu <arthurtt08@gmail.com>
2024-09-20 10:38:22 -0300

    adicionando favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,12 +10,16 @@ const fonte = JetBrains_Mono({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Portfólio - Arthur Gomes",
-  description: "",
+  description:
+    "Portfóliio sobre o Arthur Gomes dos Santos Desenvolvedor FullStack, com formação em Análise e Desenvolvimento de sistema e Blog com suas principais ideias",
+  icons: {
+    icon: "/assets/favicon.ico", // Caminho correto para o favicon
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br" className="dark">
+    <html lang="pt-BR" className="dark">
       <body
         className={`${fonte.className} bg-colors-backgroundClaro dark:bg-colors-background  antialiased h-screen`}
       >
