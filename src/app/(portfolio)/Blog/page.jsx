d882100476568abd100da93c7b6dd6749739d469
Arttutu <arthurtt08@gmail.com>
2024-10-24 19:29:03 -0300

    update

--- src/app/(portfolio)/Blog/page.jsx
+++ src/app/(portfolio)/Blog/page.jsx
@@ -1,4 +1,3 @@
-"use client"
 import BlogItem from "@/app/Componentes/BlogItem"
 import Titles from "@/app/Componentes/Titles"
 import { createClient } from "@/prismicio"
@@ -10,26 +9,24 @@ export default async function Blog() {
   })
 
   return (
-    <section className="bg-colors-background2 p-4 mt-4">
-      {Blog?.length > 0 && (
-        <>
-          <div className="p-4">
-            <Titles title={"Meu Bloge"} />
-            <p className="text-lg dark:text-colors-paragrafo text-colors-paragrafo2">
-              Meu bloge com dicas de tecnologias e um pouco sobre minhas
-              trajetórias
-            </p>
-          </div>
-          <div className="flex justify-end p-4 gap-2">
-            <span className="text-lg dark:text-colors-paragrafo text-colors-paragrafo2">
-              {Blog.length}/{Blog.length} postagem(s)
-            </span>
-          </div>
-        </>
-      )}
-      {Blog?.map((blogItem) => (
-        <BlogItem key={blogItem.id} blog={blogItem} />
-      ))}
+    <section className="dark:bg-colors-background2 bg-colors-backgroundClar2 rounded-none md:rounded-lg p-4 mt-4">
+      <>
+        <div className="p-4">
+          <Titles title={"Meu Bloge"} />
+          <p className="text-lg dark:text-colors-paragrafo text-colors-paragrafo2">
+            Meu blog com dicas de tecnologias e um pouco sobre minhas
+            trajetórias
+          </p>
+        </div>
+        <div className="flex justify-end p-4 gap-2">
+          <span className="text-lg dark:text-colors-paragrafo text-colors-paragrafo2">
+            {Blog?.length}/{Blog?.length} postagem(s)
+          </span>
+        </div>
+        {Blog?.map((blogItem) => (
+          <BlogItem key={blogItem.id} blog={blogItem} />
+        ))}
+      </>
     </section>
   )
 }
