dbf5943862dfea47487330799ebec175620ea1be
Arttutu <arthurtt08@gmail.com>
2025-04-08 22:07:13 -0300

    fix

--- src/app/(portfolio)/Projetos/page.jsx
+++ src/app/(portfolio)/Projetos/page.jsx
@@ -8,7 +8,6 @@ import BodySessao from "@/app/Componentes/UI/BodySessao"
 export default async function Projetos() {
   const prismicClient = createClient("arthurgomes", {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-    fetchOptions: { cache: "no-store" },
   })
   const projetos = await prismicClient
     .getAllByType("projetos", {
